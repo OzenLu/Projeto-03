3f9abc5dce89f0997187efdc423352fcfa075ef6
Luan Cerqueira <luancerqueira.s2@gmail.com>
2024-05-17 12:03:51 -0300

    Update Principal.java

--- projeto03/Principal.java
+++ projeto03/Principal.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.projeto03;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author 12724141297
- */
+
 public class Principal {
     public static void main(String[] args) {
         ArrayList<Produtos> listaDeProdutos = new ArrayList();
